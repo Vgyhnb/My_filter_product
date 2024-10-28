0ff8ab925f9b1cea861751242bf3b5a61ed29593
DariaDDD <vgyhnb1234@gmail.com>
2024-10-28 11:42:07 +0300

    first commit

---
+++ javascript.js
@@ -0,0 +1,31 @@
+const filterBox = document.querySelectorAll('.box');
+const navBox = document.querySelectorAll('nav ul li');
+
+const filterGoods = filterClass => {
+
+    navBox.forEach(item => item.classList.remove('active'));
+    const active = document.querySelector(`[data-f="${filterClass}"]`);
+    if (active) active.classList.add('active');
+
+
+    filterBox.forEach(item => {
+        item.classList.remove('hide');
+        if (!item.classList.contains(filterClass) && filterClass !== 'all') {
+            item.classList.add('hide');
+        }
+    })
+}
+
+document.querySelector('nav').addEventListener('click', event => {
+    if (event.target.tagName !== 'LI') return;
+
+    let filterClass = event.target.dataset['f'];
+    filterGoods(filterClass);
+    window.location.hash = event.target.dataset['f'];
+});
+
+if (window.location.hash) {
+    console.log(window.location.hash);
+    filterGoods(window.location.hash.slice());
+    console.log(window.location.hash);
+}
